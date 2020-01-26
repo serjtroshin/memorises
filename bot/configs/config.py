fd5d4a59dcedd7ff758365dfb3d4c3b961461b57
istar <serj.troshin2013@yandex.ru>
2020-01-26 20:04:44 +0300

    update config

--- bot/configs/config.py
+++ bot/configs/config.py
@@ -4,12 +4,13 @@ import os
 import yaml
 
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.yaml")
+url = os.environ.get("DATABASE_URL")
 
 
 class Config:
     @staticmethod
     def get_config():
-        if os.path.isfile(CONFIG_PATH):
+        if url is None:
             return yaml.load(open(CONFIG_PATH, "r"), Loader=yaml.Loader)
         else:
             return json.loads(os.environ["config"])
