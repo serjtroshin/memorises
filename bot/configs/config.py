34c36ca243bb52cf20d673bb8070e6423f4ddbaf
BuildTools <magnickolas@gmail.com>
2020-01-26 17:08:41 +0300

    add dummy config file

--- bot/configs/config.py
+++ bot/configs/config.py
@@ -1,12 +1,13 @@
+import yaml
 import json
 import os
 
-CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.json")
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.yaml")
 
 class Config:
     @staticmethod
     def get_config():
         if os.path.isfile(CONFIG_PATH):
-            return json.load(open(CONFIG_PATH, "r"))
+            return yaml.load(open(CONFIG_PATH, "r"), Loader=yaml.Loader)
         else:
             return json.loads(os.environ["config"])
